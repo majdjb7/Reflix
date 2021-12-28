728d6490662bb7024b78ea6915481f24a5b7edab
majdjb7 <majdjd171@gmail.com>
2021-12-28 19:17:24 +0200

    Basic landing page (with css) done

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Landing extends Component {
     render() {
+        console.log(this.props.state.users)
+        let usersArray = this.props.state.users
         return (
             <div>
-               <h1>Landing</h1> 
+                <h1 id="watch-title">Who's Watching</h1>
+                <div className='users-container'>
+                    {usersArray.map(u => {
+                        return (
+                            <Link to={`/catalog`}>
+                                <div className="user-container">
+                                    <img className='user-img' src={u.img} alt="" />
+                                    <h1 className='user-name'>{u.name}</h1>
+                                </div>
+                            </Link>
+                        )
+                    })
+                    }
+                </div>
             </div>
         )
     }
